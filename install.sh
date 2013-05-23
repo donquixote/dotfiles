d351a32689f181ccc1021fa3731c8dea5a2aeda1
Kasper Souren <kasper@guaka.org>
2013-05-23 17:38:57 +0200

    bin/cwatch

--- install.sh
+++ install.sh
@@ -5,10 +5,13 @@ cd "$(dirname "$0")"
 # Backup whatever's there
 mkdir -p ~/.dotbackups/
 
-rm *~
+rm .[a-z]*~
 for dot in .[a-z]*; do
     if [ "$dot" != ".git" ]; then
 	mv ~/$dot ~/.dotbackups/
 	ln -s ~/dotfiles/$dot ~
     fi
 done
+
+cd
+ln -s "$(dirname "$0")"/bin
